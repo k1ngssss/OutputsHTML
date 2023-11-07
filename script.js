437935c30fe4afc57ca6627ebf970547e190072c
k1ngssss <146873222+k1ngssss@users.noreply.github.com>
2023-11-07 18:47:27 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = 20;
+let b = 3;
+let c = a * b;
+let d = 40
+let expressão = 30 + (c - d)/2;
+document.getElementById("cálculo").innerHTML =
+"O valor da expressão é: " + expressão; 
+
+alert("O valor da expressão é: " + expressão);
+
+console.log("O valor da expressão é: " + expressão);
